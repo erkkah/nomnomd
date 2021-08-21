5f5f952924c96836c89c843f321d920887f48603
Erik Agsjö <erik.agsjo@gmail.com>
2021-08-21 15:52:42 +0200

    Syntax highlighting and themes

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,22 @@ import { mkdirSync, readFileSync, writeFileSync } from "fs";
 import { basename, join } from "path";
 
 import MarkdownIt from "markdown-it";
+import Highlight from "highlight.js";
 import arg from "arg";
 
 import * as pkg from "../package.json";
 import { dirListing } from "./dirlisting";
 import { makeInclude } from "./include";
 import { nomnomlIt } from "./nomnoml";
+import { wrapContent } from "./html";
 
 export async function main(args: string[]) {
     const parsed = arg(
         {
             "--help": Boolean,
             "--out": String,
+            "--theme": String,
+            "--hltheme": String,
         },
         {
             argv: args,
@@ -27,30 +31,55 @@ export async function main(args: string[]) {
         console.log(`nomnomd version ${pkg.version}
 
 Usage:
-    nomnomd [--help|--out DIR] <files..>
+    nomnomd [--help] [--out DIR] [--theme FILE] [--hltheme <hljs-theme>] <files..>
         `);
         process.exit(0);
     }
 
-    processFiles(files, target);
+    const themeFile = parsed["--theme"];
+    let themeCSS = "";
+    if (themeFile) {
+        themeCSS = readFileSync(themeFile).toString();
+    }
+    const codeTheme = parsed["--hltheme"];
+    processFiles(files, target, themeCSS, codeTheme);
 }
 
 function getMarkdownWithPlugins(): MarkdownIt {
-    const md = MarkdownIt();
+    const md = MarkdownIt({
+        highlight: (str, lang) => {
+            if (lang && Highlight.getLanguage(lang)) {
+                try {
+                    return (
+                        '<pre class="hljs"><code>' +
+                        Highlight.highlight(str, {
+                            language: lang,
+                            ignoreIllegals: true,
+                        }).value +
+                        "</code></pre>"
+                    );
+                } catch (_) {
+                    //
+                }
+            }
+            return "";
+        },
+    });
     md.use(nomnomlIt);
     md.use(dirListing);
     md.use(makeInclude(getMarkdownWithPlugins));
     return md;
 }
 
-function processFiles(files: string[], target: string) {
+function processFiles(files: string[], target: string, themeCSS?: string, codeTheme?: string) {
     const md = getMarkdownWithPlugins();
 
     mkdirSync(target, { recursive: true });
 
     for (const file of files) {
         const text = readFileSync(file);
-        const markdown = md.render(text.toString());
-        writeFileSync(join(target, basename(file, ".md") + ".html"), markdown);
+        const content = md.render(text.toString());
+        const html = wrapContent(content, { themeCSS, codeTheme });
+        writeFileSync(join(target, basename(file, ".md") + ".html"), html);
     }
 }
